307fec09dc3ede6d8623aa03d772b21a51386631
AliceMillers <Panterka13@yandex.ru>
2016-12-20 18:00:03 +0100

    We have marks when an enemy fire in our ship and new parameter - size of ship on horizontally.

--- class_field.py
+++ class_field.py
@@ -1,6 +1,3 @@
-from random import randint
-
-
 class Field(object):
     board = []
     def __init__(self):
@@ -11,20 +8,29 @@ class Field(object):
         for row in self.board:
             print(" ".join(row))
 
-    def add_ship(self, x, y):
-        self.board[x][y] = "X"
+    def add_ship(self,size, x, y):
+        for i in range(size):
+            self.board[x-1][y-1] = "-"
+            y += 1
+
 
     def fire(self, x, y):
-        if self.board[x][y] == "X":
+        print("Your turn is: "+ str(x) + "," + str(y))
+        if self.board[x-1][y-1] == "-":
+            self.board[x - 1][y - 1] = "X"
+            print("Congratulations! You sunk my battleship!")
             return True
         else:
+            self.board[x - 1][y - 1] = "*"
+            print("You missed my battleship!")
             return False
 
-
 field = Field()
-field.add_ship(5, 5)
+field.add_ship(3, 5, 5)
 field.print_field()
 print(field.fire(5, 5))
+field.print_field()
 print(field.fire(1, 1))
+field.print_field()
 
 
