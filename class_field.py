ddfcf5b41641196c751567b119188a47908aa234
AliceMillers <Panterka13@yandex.ru>
2017-01-30 17:26:21 +0100

    #1 Another ships can't stand near the ship which set already.

--- class_field.py
+++ class_field.py
@@ -9,15 +9,25 @@ class Field(object):
             print(" ".join(row))
 
     def add_ship(self,size, x, y):
-        self.i = y
         for i in range(size):
-            self.board[x-1][self.i-1] = "-"
-            self.i += 1
+            if self.board[x - 1][y + i - 1] == "-":
+                print("Sorry. These cells are occupied!")
+                return
+        for i in range(size):
+            if self.board[x - 2][y + i - 2] != "O" or self.board[x - 1][y + i - 2] != "O" \
+                    or self.board[x][y + i - 2] != "O" or self.board[x - 2][y + i - 1] != "O" \
+                    or self.board[x][y + i - 1] != "O" or self.board[x - 2][y + i] != "O" \
+                    or self.board[x - 1][y + i] != "O" or self.board[x][y + i] != "O":
+                print("Sorry. Another ship stands near here.")
+                return
+        for i in range(size):
+            self.board[x - 1][y + i - 1] = "-"
+
 
 
     def fire(self, x, y):
         print("Your turn is: "+ str(x) + "," + str(y))
-        if self.board[x-1][y-1] == "-":
+        if self.board[x - 1][y - 1] == "-":
             self.board[x - 1][y - 1] = "X"
             print("Congratulations! You've hit!")
             return True
@@ -33,5 +43,6 @@ print(field.fire(5, 5))
 field.print_field()
 print(field.fire(1, 1))
 field.print_field()
-
-
+print("Add another ship")
+field.add_ship(3, 6, 1)
+field.print_field()
