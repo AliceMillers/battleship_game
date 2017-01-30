6c3e0e95d688fb95f88ab88a1ee45a706604229b
AliceMillers <Panterka13@yandex.ru>
2017-01-30 17:27:08 +0100

    #2 I've changed add_ship on add_ship_horizontal and made method add_ship_vertical. Now you can add ship which can be situated horizontally and vertically.

--- class_field.py
+++ class_field.py
@@ -8,13 +8,11 @@ class Field(object):
         for row in self.board:
             print(" ".join(row))
 
-    def add_ship(self,size, x, y):
+    def add_ship_horizontal(self,size, x, y):
         for i in range(size):
             if self.board[x - 1][y + i - 1] == "-":
                 print("Sorry. These cells are occupied!")
-                return
-        for i in range(size):
-            if self.board[x - 2][y + i - 2] != "O" or self.board[x - 1][y + i - 2] != "O" \
+            elif self.board[x - 2][y + i - 2] != "O" or self.board[x - 1][y + i - 2] != "O" \
                     or self.board[x][y + i - 2] != "O" or self.board[x - 2][y + i - 1] != "O" \
                     or self.board[x][y + i - 1] != "O" or self.board[x - 2][y + i] != "O" \
                     or self.board[x - 1][y + i] != "O" or self.board[x][y + i] != "O":
@@ -23,7 +21,18 @@ class Field(object):
         for i in range(size):
             self.board[x - 1][y + i - 1] = "-"
 
-
+    def add_ship_vertical(self,size, x, y):
+        for i in range(size):
+            if self.board[x + i - 1][y - 1] == "-":
+                print("Sorry. These cells are occupied!")
+            elif self.board[x + i][y - 1] != "O" or self.board[x + i][y] != "O" \
+                    or self.board[x + i - 1][y] != "O" or self.board[x + i - 2][y] != "O" \
+                    or self.board[x + i - 2][y - 1] != "O" or self.board[x + i - 2][y - 2] != "O" \
+                    or self.board[x + i - 1][y - 2] != "O" or self.board[x + i][y - 2] != "O":
+                print("Sorry. Another ship stands near here.")
+                return
+        for i in range(size):
+            self.board[x + i - 1][y - 1] = "-"
 
     def fire(self, x, y):
         print("Your turn is: "+ str(x) + "," + str(y))
@@ -37,12 +46,14 @@ class Field(object):
             return False
 
 field = Field()
-field.add_ship(3, 5, 5)
+field.add_ship_horizontal(3, 5, 5)
+field.add_ship_vertical(2, 2, 2)
 field.print_field()
 print(field.fire(5, 5))
 field.print_field()
 print(field.fire(1, 1))
 field.print_field()
 print("Add another ship")
-field.add_ship(3, 6, 1)
+field.add_ship_horizontal(3, 7, 1)
+field.add_ship_vertical(2, 1, 3)
 field.print_field()
