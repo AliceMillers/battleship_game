c4cba2b64fca1c938f9016f3d58d97834ca75c43
AliceMillers <Panterka13@yandex.ru>
2017-02-09 14:34:42 +0100

    #3 I've changed board = [] on self.board = [] in the start of class.

--- class_field.py
+++ class_field.py
@@ -1,6 +1,6 @@
 class Field(object):
-    board = []
     def __init__(self):
+        self.board = []
         for x in range(10):
             self.board.append(["O"] * 10)
     print ("Let's play Battleship!")
@@ -9,30 +9,42 @@ class Field(object):
             print(" ".join(row))
 
     def add_ship_horizontal(self,size, x, y):
-        for i in range(size):
-            if self.board[x - 1][y + i - 1] == "-":
-                print("Sorry. These cells are occupied!")
-            elif self.board[x - 2][y + i - 2] != "O" or self.board[x - 1][y + i - 2] != "O" \
-                    or self.board[x][y + i - 2] != "O" or self.board[x - 2][y + i - 1] != "O" \
-                    or self.board[x][y + i - 1] != "O" or self.board[x - 2][y + i] != "O" \
-                    or self.board[x - 1][y + i] != "O" or self.board[x][y + i] != "O":
-                print("Sorry. Another ship stands near here.")
-                return
-        for i in range(size):
-            self.board[x - 1][y + i - 1] = "-"
+        if size < 5:
+            for i in range(size):
+                if self.board[x - 1][y + i - 1] == "-":
+                    print("Sorry. These cells are occupied!")
+                elif self.board[x - 2][y + i - 2] != "O" or self.board[x - 1][y + i - 2] != "O" \
+                        or self.board[x][y + i - 2] != "O" or self.board[x - 2][y + i - 1] != "O" \
+                        or self.board[x][y + i - 1] != "O" or self.board[x - 2][y + i] != "O" \
+                        or self.board[x - 1][y + i] != "O" or self.board[x][y + i] != "O":
+                    print("Sorry. Another ship stands near here.")
+                    return
+            for i in range(size):
+                self.board[x - 1][y + i - 1] = "-"
+        else:
+            print("The size of the inputted ship is too big. Max size must be 4 cells")
 
     def add_ship_vertical(self,size, x, y):
-        for i in range(size):
-            if self.board[x + i - 1][y - 1] == "-":
-                print("Sorry. These cells are occupied!")
-            elif self.board[x + i][y - 1] != "O" or self.board[x + i][y] != "O" \
-                    or self.board[x + i - 1][y] != "O" or self.board[x + i - 2][y] != "O" \
-                    or self.board[x + i - 2][y - 1] != "O" or self.board[x + i - 2][y - 2] != "O" \
-                    or self.board[x + i - 1][y - 2] != "O" or self.board[x + i][y - 2] != "O":
-                print("Sorry. Another ship stands near here.")
-                return
-        for i in range(size):
-            self.board[x + i - 1][y - 1] = "-"
+        if size < 5:
+            for i in range(size):
+                if self.board[x + i - 1][y - 1] == "-":
+                    print("Sorry. These cells are occupied!")
+                elif self.board[x + i][y - 1] != "O" or self.board[x + i][y] != "O" \
+                        or self.board[x + i - 1][y] != "O" or self.board[x + i - 2][y] != "O" \
+                        or self.board[x + i - 2][y - 1] != "O" or self.board[x + i - 2][y - 2] != "O" \
+                        or self.board[x + i - 1][y - 2] != "O" or self.board[x + i][y - 2] != "O":
+                    print("Sorry. Another ship stands near here.")
+                    return
+            for i in range(size):
+                self.board[x + i - 1][y - 1] = "-"
+        else:
+            print("The size of the inputted ship is too big. Max size must be 4 cells")
+
+    def add_ship(self, size, x, y, direction):
+        if direction.lower() == "h":
+            self.add_ship_horizontal(size, x, y)
+        elif direction.lower() == "v":
+            self.add_ship_vertical(size, x, y)
 
     def fire(self, x, y):
         print("Your turn is: "+ str(x) + "," + str(y))
@@ -53,7 +65,6 @@ print(field.fire(5, 5))
 field.print_field()
 print(field.fire(1, 1))
 field.print_field()
-print("Add another ship")
 field.add_ship_horizontal(3, 7, 1)
-field.add_ship_vertical(2, 1, 3)
+field.add_ship_vertical(4, 1, 3)
 field.print_field()
