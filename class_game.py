83525c85058d3f0a0528067999d6e19ba40c2ba2
AliceMillers <Panterka13@yandex.ru>
2017-02-11 00:23:36 +0100

    #3 I've refactored the code.

--- class_game.py
+++ class_game.py
@@ -6,10 +6,10 @@ class Game(object):
 
      def set_players_ships(self):
          for i in range(10):
-             self.answer_set_ships = input("Do you want to set ship? ")
-             if self.answer_set_ships.lower() == "y":
-                 self.answer_set_ships  = input("Please input your coordinates of ship (size x y direction): ")
-                 [size, x, y, direction] = self.answer_set_ships.split(" ")
+             answer_set_ships = input("Do you want to set ship? ")
+             if answer_set_ships.lower() == "y":
+                 answer_set_ships  = input("Please input your coordinates of ship (size x y direction): ")
+                 [size, x, y, direction] = answer_set_ships.split(" ")
                  self.players_field.add_ship(int(size), int(x), int(y), direction)
                  self.players_field.print_field()
                  print("Your ship was set.")
@@ -19,12 +19,12 @@ class Game(object):
 
      def start(self):
          self.set_players_ships()
-         self.answer_start = input("Do you want fire? ")
-         if self.answer_start.lower() == "y":
-             self.answer_start = input("Please input coordinates of fire (x y): ")
-             [x, y] = self.answer_start.split(" ")
-             self.players_field.fire(int(x), int(y))
-             self.players_field.print_field()
+         answer_start = input("Do you want fire? ")
+         if answer_start.lower() == "y":
+             answer_start = input("Please input coordinates of fire (x y): ")
+             [x, y] = answer_start.split(" ")
+             self.computers_field.fire(int(x), int(y))
+             self.computers_field.print_field()
          else:
              print("Come on! Don't you want to play game?")
 
