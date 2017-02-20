9292740aa585c52fe6de075e81dcb64721abb642
AliceMillers <Panterka13@yandex.ru>
2017-02-20 21:01:43 +0100

    Fix checking edge ceils.

--- class_field.py
+++ class_field.py
@@ -8,15 +8,25 @@ class Field(object):
         for row in self.board:
             print(" ".join(row))
 
+    def occupied(self, x, y):
+        if self.in_field(x, y):
+            return self.board[x][y] != "O"
+        else:
+            return False
+
+    def in_field(self, x, y):
+        return 0 <= x < 10 and 0 <= y < 10
+
     def add_ship_horizontal(self,size, x, y):
         if size < 5:
             for i in range(size):
-                if self.board[x - 1][y + i - 1] == "-":
-                    print("Sorry. These cells are occupied!")
-                elif self.board[x - 2][y + i - 2] != "O" or self.board[x - 1][y + i - 2] != "O" \
-                        or self.board[x][y + i - 2] != "O" or self.board[x - 2][y + i - 1] != "O" \
-                        or self.board[x][y + i - 1] != "O" or self.board[x - 2][y + i] != "O" \
-                        or self.board[x - 1][y + i] != "O" or self.board[x][y + i] != "O":
+                if not self.in_field(x - 1,  y + i - 1) or self.board[x - 1][y + i - 1] == "-":
+                    print("Sorry. These cells are occupied or out of the field!")
+                    return
+                elif self.occupied(x -2, y + i -2) or self.occupied(x - 1, y + i - 2) \
+                        or self.occupied(x, y + i - 2) or self.occupied(x - 2, y + i - 1) \
+                        or self.occupied(x, y + i - 1) or self.occupied(x - 2, y + i) \
+                        or self.occupied(x - 1, y + i) or self.occupied(x, y + i):
                     print("Sorry. Another ship stands near here.")
                     return
             for i in range(size):
@@ -27,12 +37,13 @@ class Field(object):
     def add_ship_vertical(self,size, x, y):
         if size < 5:
             for i in range(size):
-                if self.board[x + i - 1][y - 1] == "-":
-                    print("Sorry. These cells are occupied!")
-                elif self.board[x + i][y - 1] != "O" or self.board[x + i][y] != "O" \
-                        or self.board[x + i - 1][y] != "O" or self.board[x + i - 2][y] != "O" \
-                        or self.board[x + i - 2][y - 1] != "O" or self.board[x + i - 2][y - 2] != "O" \
-                        or self.board[x + i - 1][y - 2] != "O" or self.board[x + i][y - 2] != "O":
+                if not self.in_field(x + i - 1, y - 1) or self.board[x + i - 1][y - 1] == "-":
+                    print("Sorry. These cells are occupied or out of the field!")
+                    return
+                elif self.occupied(x + i, y - 1) or self.occupied(x + i, y) \
+                        or self.occupied(x + i - 1, y) or self.occupied(x + i - 2, y) \
+                        or self.occupied(x + i - 2, y - 1) or self.occupied(x + i - 2, y - 2) \
+                        or self.occupied(x + i - 1, y - 2) or self.occupied(x + i, y - 2):
                     print("Sorry. Another ship stands near here.")
                     return
             for i in range(size):
@@ -47,7 +58,6 @@ class Field(object):
             self.add_ship_vertical(size, x, y)
 
     def fire(self, x, y):
-        print("Your turn is: "+ str(x) + "," + str(y))
         if self.board[x - 1][y - 1] == "-":
             self.board[x - 1][y - 1] = "X"
             print("Congratulations! You've hit!")
@@ -58,13 +68,13 @@ class Field(object):
             return False
 
 field = Field()
-field.add_ship_horizontal(3, 5, 5)
-field.add_ship_vertical(2, 2, 2)
-field.print_field()
-print(field.fire(5, 5))
-field.print_field()
-print(field.fire(1, 1))
-field.print_field()
-field.add_ship_horizontal(3, 7, 1)
-field.add_ship_vertical(4, 1, 3)
-field.print_field()
+#field.add_ship_horizontal(3, 5, 5)
+#field.add_ship_vertical(2, 2, 2)
+#field.print_field()
+#print(field.fire(5, 5))
+#field.print_field()
+#print(field.fire(1, 1))
+#field.print_field()
+#field.add_ship_horizontal(3, 7, 1)
+#field.add_ship_vertical(4, 1, 3)
+#field.print_field()
