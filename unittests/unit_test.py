e7051b4bb4a9588ced27f63eee7ec7eb53e8ea27
AliceMillers <Panterka13@yandex.ru>
2017-02-21 15:06:42 +0100

    Added more unit tests which check adding ships in the field.

--- unittests/unit_test.py
+++ unittests/unit_test.py
@@ -1,7 +1,18 @@
 import unittest
 from class_field import *
 
-class MyTest(unittest.TestCase):
-    def test(self):
-        field = Field()
-        field.add_ship(1, 10, 10, "v")
+
+class FieldTest(unittest.TestCase):
+    field = Field()
+
+    def test_in_rigth_down_corner(self):
+        self.assertIsNone(field.add_ship(1, 10, 10, "v"))
+
+    def test_in_rigth_up_corner(self):
+        self.assertIsNone(field.add_ship(1, 10, 1, "v"))
+
+    def test_in_left_up_corner(self):
+        self.assertIsNone(field.add_ship(1, 1, 1, "v"))
+
+    def test_in_left_down_corner(self):
+        self.assertIsNone(field.add_ship(1, 1, 10, "v"))
