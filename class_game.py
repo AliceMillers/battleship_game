de9b50dbeecdf159962b5afbf8a0f9c20bf88804
AliceMillers <Panterka13@yandex.ru>
2017-02-27 23:15:04 +0100

    Alpha version is ready: game logic implemented.

--- class_game.py
+++ class_game.py
@@ -14,23 +14,26 @@ def suppress_stdout():
             sys.stdout = old_stdout
 
 class Game(object):
+
+    TOTAL_NUMBER_OF_CELLS = 20
+
     def __init__(self):
         self.players_field = Field()
         self.computers_field = Field()
 
     def set_players_ships(self):
-        for i in range(1):
-            answer_set_ships = input("Do you want to set ship?(y/n) ")
-            if answer_set_ships.lower() == "y":
+        answer_set_ships = input("Do you want to set ship?(y/n) ")
+        if answer_set_ships.lower() == "y":
+            for i in range(10):
                 answer_set_ships  = input("Please input your coordinates of ship (size x y direction): ")
                 [size, x, y, direction] = answer_set_ships.split(" ")
                 self.players_field.add_ship(int(size), int(x), int(y), direction)
                 self.players_field.print_field()
-            elif answer_set_ships.lower() == "n":
-                return False
-            else:
-                print("You shoud set your ships if you want to play.")
-                return False
+        elif answer_set_ships.lower() == "n":
+            return False
+        else:
+            print("You shoud set your ships if you want to play.")
+            return False
         print("Your ships were set.")
         return True
 
@@ -61,34 +64,37 @@ class Game(object):
         with suppress_stdout():
             if not self.set_computers_ships():
                 return
-        self.computers_field.print_field()
         print("Computers ships were set.")
 
-        fire_again_user = True
-        fire_again_computer = True
-        while fire_again_user:
-            answer_start = input("Do you want fire?(y/n) ")
-            if answer_start.lower() == "y":
+        killed_cell_user = 0
+        killed_cell_computer = 0
+        while not killed_cell_user == Game.TOTAL_NUMBER_OF_CELL and not killed_cell_computer == Game.TOTAL_NUMBER_OF_CELL:
+            fire_again_user = True
+            fire_again_computer = True
+            while fire_again_user and not killed_cell_user == Game.TOTAL_NUMBER_OF_CELL:
                 answer_start = input("Please input coordinates of fire (x y): ")
                 [x, y] = answer_start.split(" ")
                 print("Your turn is: " + str(x) + "," + str(y))
                 if self.computers_field.fire(int(x), int(y)):
                     fire_again_user = True
+                    killed_cell_user += 1
                 else:
                     fire_again_user = False
                 self.computers_field.print_field()
-            else:
-                print("Come on! Don't you want to play game?")
-                fire_again_user = False
-        while fire_again_computer:
-            computer_fire_x = random.randint(1, 10)
-            computer_fire_y = random.randint(1, 10)
-            print("Computer's turn is: " + str(computer_fire_x) + "," + str(computer_fire_y))
-            if self.players_field.fire(computer_fire_x, computer_fire_y):
-                fire_again_computer = True
-            else:
-                fire_again_computer = False
-            self.players_field.print_field()
+
+            if killed_cell_user == Game.TOTAL_NUMBER_OF_CELL:
+                break
+
+            while fire_again_computer and not killed_cell_computer == Game.TOTAL_NUMBER_OF_CELL:
+                computer_fire_x = random.randint(1, 10)
+                computer_fire_y = random.randint(1, 10)
+                print("Computer's turn is: " + str(computer_fire_x) + "," + str(computer_fire_y))
+                if self.players_field.fire(computer_fire_x, computer_fire_y):
+                    fire_again_computer = True
+                    killed_cell_computer += 1
+                else:
+                    fire_again_computer = False
+                self.players_field.print_field()
 
 game = Game()
 game.start()
